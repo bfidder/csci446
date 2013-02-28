eeded8811c6d83ae7cebda97c135b414b94dfe87
Brianna Fidder <bfidder@mines.edu>
2013-02-27 18:44:34 -0700

    was a bit confused on what the project was supposed to be called (project04 or depot) so I accidentally readded those files. Added the scaffolding for the db.

--- project04/config/initializers/secret_token.rb
+++ project04/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '75fcb7c8ba9282c683d556cfb5f24b5964114f57e480e4a68bab7ed8276b5e9d317c459736499849b921841b6c3d21201fb8d65600d9da8896a6bfb2bb6bcf1b'
+Depot::Application.config.secret_token = '2f71ac7315342bae60cd9233d51532fb6b7750546cc16fdecc1abd65dfa1505aa6dc4c81021015de28c3c08c232243959351fda15f7857009cd71a693810217d'
